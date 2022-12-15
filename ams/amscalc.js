ad826579e687ac5619a3e8884eea3ef43cad3b4e
iotux <46082385+iotux@users.noreply.github.com>
2022-12-15 09:43:32 +0100

    Update amscalc.js

--- ams/amscalc.js
+++ ams/amscalc.js
@@ -9,54 +9,46 @@ const config = yaml.load(configFile);
 const debug = config.DEBUG;
 
 const amsCalc = {
-
-  lastMeterConsumption: 0,
-  lastMeterProduction: 0,
-  accumulatedConsumption: 0,
-  accumulatedProduction: 0,
   // Not used
   lastMeterConsumptionReactive: 0,
   lastMeterProductionReactive: 0,
 
   calc: function (obj) {
 
-    // Energy consumed last hour
-    if (this.lastMeterConsumption > 0) {
-      obj.accumulatedConsumptionLastHour = (obj.lastMeterConsumption - this.lastMeterConsumption).toFixed(3) * 1;
-      this.accumulatedConsumption += obj.accumulatedConsumptionLastHour;
+    // Energy consumtion
+    if (db.get("lastMeterConsumption") > 0) {
+      obj.accumulatedConsumptionLastHour = (obj.lastMeterConsumption - db.get("lastMeterConsumption")).toFixed(3) * 1;
+      db.set("accumulatedConsumptionLastHour", obj.accumulatedConsumptionLastHour);
+      db.set("lastMeterConsumption", obj.lastMeterConsumption);
+      obj.accumulatedConsumption = (obj.lastMeterConsumption - db.get("prevDayMeterConsumption")).toFixed(3) * 1;
+      db.set("accumulatedConsumption", obj.accumulatedConsumption);
+    } else {
+      if (db.get("prevDayMeterConsumption") === 0)
+        db.set("prevDayMeterConsumption", obj.lastMeterConsumption);
     }
-      else obj.accumulatedConsumptionLastHour = 0;
-    this.lastMeterConsumption = obj.lastMeterConsumption;
-
-    if (this.lastMeterProduction > 0) {
-      obj.accumulatedProductionLastHour = (obj.lastMeterProduction - this.lastMeterProduction).toFixed(3) * 1;
-      this.accumulatedProduction += obj.accumulatedConsumptionLastHour;
+    // Energy production
+    if (db.get("lastMeterProduction") > 0) {
+      obj.accumulatedProductionLastHour = (obj.lastMeterProduction - db.get("lastMeterProduction")).toFixed(3) * 1;
+      db.set("accumulatedProductionLastHour", obj.accumulatedProductionLastHour);
+      db.set("lastMeterProduction", obj.lastMeterProduction);
+      obj.accumulatedProduction = (obj.lastMeterProduction - db.get("prevDayMeterProduction")).toFixed(3) * 1;
+      db.set("accumulatedProduction", obj.accumulatedProduction);
+    } else {
+      if (db.get("prevDayMeterProduction") === 0)
+        db.set("prevDayMeterProduction", obj.lastMeterProduction);
     }
-      else obj.accumulatedProductionLastHour = 0;
-    this.lastMeterProduction = obj.lastMeterProduction;
-
-    obj.accumulatedConsumption = this.accumulatedConsumption.toFixed(3) * 1;
-    obj.accumulatedProduction = this.accumulatedProduction.toFixed(3) * 1;
    
-    db.set("lastMeterConsumption", obj.lastMeterConsumption);
-    db.set("lastMeterProduction", obj.lastMeterProduction);
     db.set("lastMeterConsumptionReactive", obj.lastMeterConsumptionReactive);
     db.set("lastMeterProductionReactive", obj.lastMeterProductionReactive);
 
-    db.set("accumulatedConsumption", obj.accumulatedConsumption);
-    db.set("accumulatedProduction", obj.accumulatedProduction);
-    db.set("accumulatedConsumptionLastHour", obj.accumulatedConsumptionLastHour);
-    db.set("accumulatedProductionLastHour", obj.accumulatedProductionLastHour);
     //db.set('minPower', obj.minPower);
     //db.set('maxPower', obj.maxPower);
     //db.set('averagePower', obj.averagePower);
 
     if (obj.meterDate.substr(11, 8) === "00:00:10") {
       // https://youtu.be/j81Vx-0uM0k
-      this.accumulatedConsumption = 0;
-      this.accumulatedProduction = 0;
-      this.accumulatedConsumptionLastHour = 0;
-      this.accumulatedProductionLastHour = 0;
+      db.set("prevDayMeterConsumption", obj.lastMeterConsumption);
+      db.set("prevDayMeterProduction", obj.lastMeterProduction);
       db.set("accumulatedConsumption", 0);
       db.set("accumulatedProduction", 0);
       db.set("accumulatedConsumptionLastHour", 0);
