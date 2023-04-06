42a46acaf51ae0f66b8fae66e24c397478786713
iotux <iotux@tux.no>
2023-04-06 07:22:45 +0200

    Revert "Several changes"
    This reverts commit 70970d3aec1183c30504d26f52f89f60f43789be.

--- ams/aidon.js
+++ ams/aidon.js
@@ -1,170 +1,149 @@
 const yaml = require("yamljs");
 const configFile = "./config.yaml";
-
-const amsCalc = require('../ams/amscalc.js')
-const db = require('../misc/dbinit.js')
-const { event } = require('../misc/misc.js')
-const { hex2Dec, hex2Ascii, hasData, getAmsTime } = require('../misc/util.js')
-const { formatISO } = require('date-fns')
+const amsCalc = require('../ams/amscalc.js');
+const db = require('../misc/dbinit.js');
+const { event } = require('../misc/misc.js');
+const {
+  hex2Dec,
+  hex2Ascii,
+  hasData,
+  getAmsTime,
+} = require('../misc/util.js');
+const { formatISO } = require('date-fns');
 
 // Load broker and topics preferences from config file
 const config = yaml.load(configFile);
 const debug = config.DEBUG;
 
 // Aidon constants
-const METER_VERSION = "020209060101000281FF0A0B"; // 22
-const METER_ID = "020209060000600100FF0A10";      // 32
-const METER_MODEL = "020209060000600107FF0A04";   // 8
-const POWER = "020309060100010700FF06";           // 8
-const POWER_PRODUCTION = "020309060100020700FF06";// 8
-const POWER_REACTIVE = "020309060100030700FF06";  // 8
-const POWER_PRODUCTION_REACTIVE = "020309060100040700FF06"; // 8
-const CURRENT_L1 = "0203090601001F0700FF10";      // 4
-const CURRENT_L2 = "020309060100330700FF10";      // 4
-const CURRENT_L3 = "020309060100470700FF10";      // 4
-const VOLTAGE_PHASE_1 = "020309060100200700FF12"; // 4 signed int
-const VOLTAGE_PHASE_2 = "020309060100340700FF12"; // 4 signed int
-const VOLTAGE_PHASE_3 = "020309060100480700FF12"; // 4 signed int
-const DATE = "020209060000010000FF090C";  // 4,2,2,2,2,2
-const LAST_METER_CONSOMPTION = "020309060100010800FF06"; // 8
-const LAST_METER_PRODUCTION = "020309060100020800FF06";  // 8
-const LAST_METER_CONSOMPTION_REACTIVE = "020309060100030800FF06"; // 8
-const LAST_METER_PRODUCTION_REACTIVE = "020309060100040800FF06"; // 8
+const AIDON_CONSTANTS = {
+  METER_VERSION: "020209060101000281FF0A0B",
+  METER_ID: "020209060000600100FF0A10",
+  METER_MODEL: "020209060000600107FF0A04",
+  POWER: "020309060100010700FF06",
+  POWER_PRODUCTION: "020309060100020700FF06",
+  POWER_REACTIVE: "020309060100030700FF06",
+  POWER_PRODUCTION_REACTIVE: "020309060100040700FF06",
+  CURRENT_L1: "0203090601001F0700FF10",
+  CURRENT_L2: "020309060100330700FF10",
+  CURRENT_L3: "020309060100470700FF10",
+  VOLTAGE_PHASE_1: "020309060100200700FF12",
+  VOLTAGE_PHASE_2: "020309060100340700FF12",
+  VOLTAGE_PHASE_3: "020309060100480700FF12",
+  DATE: "020209060000010000FF090C",
+  LAST_METER_CONSOMPTION: "020309060100010800FF06",
+  LAST_METER_PRODUCTION: "020309060100020800FF06",
+  LAST_METER_CONSOMPTION_REACTIVE: "020309060100030800FF06",
+  LAST_METER_PRODUCTION_REACTIVE: "020309060100040800FF06",
+};
 
-let obj = new Object();
-
+/**
+ * Converts a hexadecimal value to a decimal value with a sign.
+ * @param {string} hex - The hexadecimal value to be converted.
+ * @returns {number} - The converted decimal value with a sign.
+ */
 function hex2DecSign(hex) {
-  let dec = parseInt(hex, 16)
+  let dec = parseInt(hex, 16);
   if ((dec & 0x8000) > 0) {
     dec = dec - 0x10000;
   }
   return dec;
 }
 
-/*
-function getMinPower(pow) {
-  if (db.get('minPower') === undefined || db.get('minPower') > pow){
-    db.set('minPower', pow);
-  }
-  return db.get('minPower');
-};
-
-function getMaxPower(pow) {
-  if (db.get('maxPower') === undefined || db.get('maxPower') < pow) {
-    db.set('maxPower', pow);
-  }
-  return db.get('maxPower');
-};
-*/
-
-const listDecode = async function (buf) {
+async function listDecode(buf) {
   let msg = new Object();
   msg.data = buf;
 
-  obj = { listType: 'list1', data: {} }
-  //obj.data.timestamp = formatISO(new Date(), { representation: 'complete' });
+  obj = { listType: 'list1', data: {} };
 
-  if (hasData(msg.data, METER_VERSION) > -1) {
-    obj.data.meterVersion = hex2Ascii(msg.data.substr(hasData(msg.data, METER_VERSION), 22))
-    obj.listType = 'list2';
-  }
-
-  if (hasData(msg.data, METER_ID) > -1) {
-    obj.data.meterID = hex2Ascii(msg.data.substr(hasData(msg.data, METER_ID), 32))
-  }
-
-  if (hasData(msg.data, METER_MODEL) > -1) {
-    obj.data.meterModel = hex2Ascii(msg.data.substr(hasData(msg.data, METER_MODEL), 8))
-  }
-
-  if (hasData(msg.data, POWER) > -1) {
-    obj.data.power = hex2Dec(msg.data.substr(hasData(msg.data, POWER), 8)) / 1000;
-  }
-
-  if (hasData(msg.data, POWER_REACTIVE) > -1) {
-    obj.data.powerReactive = hex2Dec(msg.data.substr(hasData(msg.data, POWER_REACTIVE), 8)) / 1000;
-  }
-
-  if (hasData(msg.data, POWER_PRODUCTION) > -1) {
-    obj.data.powerProduction = hex2Dec(msg.data.substr(hasData(msg.data, POWER_PRODUCTION), 8)) / 1000;
-  }
-
-  if (hasData(msg.data, POWER_PRODUCTION_REACTIVE) > -1) {
-    obj.data.powerProductionReactive = hex2Dec(msg.data.substr(hasData(msg.data, POWER_PRODUCTION_REACTIVE), 8)) / 1000;
-  }
-
-  if (hasData(msg.data, CURRENT_L1) > -1) {
-    obj.data.currentL1 = hex2DecSign(msg.data.substr(hasData(msg.data, CURRENT_L1), 4)) / 10;
-  }
-
-  if (hasData(msg.data, CURRENT_L2) > -1) {
-    obj.data.currentL2 = hex2DecSign(msg.data.substr(hasData(msg.data, CURRENT_L2), 4)) / 10;
-  }
-
-  if (hasData(msg.data, CURRENT_L3) > -1) {
-    obj.data.currentL3 = hex2DecSign(msg.data.substr(hasData(msg.data, CURRENT_L3), 4)) / 10;
-  }
-
-  if (hasData(msg.data, VOLTAGE_PHASE_1) > -1) {
-    obj.data.voltagePhase1 = hex2Dec(msg.data.substr(hasData(msg.data, VOLTAGE_PHASE_1), 4)) / 10;
-  }
-
-  if (hasData(msg.data, VOLTAGE_PHASE_2) > -1) {
-    obj.data.voltagePhase2 = hex2Dec(msg.data.substr(hasData(msg.data, VOLTAGE_PHASE_2), 4)) / 10;
-
-  }
-  if (hasData(msg.data, VOLTAGE_PHASE_3) > -1) {
-    obj.data.voltagePhase3 = hex2Dec(msg.data.substr(hasData(msg.data, VOLTAGE_PHASE_3), 4)) / 10;
-  }
-
-  if (hasData(msg.data, DATE) > -1) {
-    let i = hasData(msg.data, DATE);
-    obj.data.meterDate = getAmsTime(msg.data, i);
-    obj.listType = 'list3';
-  }
-
-  if (msg.data.includes(LAST_METER_CONSOMPTION)) {
-    obj.data.lastMeterConsumption = hex2Dec(msg.data.substr(hasData(msg.data, LAST_METER_CONSOMPTION), 8)) / 100;
-  }
-
-  if (msg.data.includes(LAST_METER_CONSOMPTION_REACTIVE)) {
-    obj.data.lastMeterConsumptionReactive = hex2Dec(msg.data.substr(hasData(msg.data, LAST_METER_CONSOMPTION_REACTIVE), 8)) / 100;
-  }
-
-  if (msg.data.includes(LAST_METER_PRODUCTION)) {
-    obj.data.lastMeterProduction = hex2Dec(msg.data.substr(hasData(msg.data, LAST_METER_PRODUCTION), 8)) / 100;
-  }
-
-  if (msg.data.includes(LAST_METER_PRODUCTION_REACTIVE)) {
-    obj.data.lastMeterProductionReactive = hex2Dec(msg.data.substr(hasData(msg.data, LAST_METER_PRODUCTION_REACTIVE), 8)) / 100;
+  for (const key in AIDON_CONSTANTS) {
+    const constant = AIDON_CONSTANTS[key];
+    const dataIndex = hasData(msg.data, constant);
+    if (dataIndex > -1) {
+      switch (key) {
+        case 'METER_VERSION':
+          obj.data.meterVersion = hex2Ascii(msg.data.substr(dataIndex, 22));
+          obj.listType = 'list2';
+          break;
+        case 'METER_ID':
+          obj.data.meterID = hex2Ascii(msg.data.substr(dataIndex, 32));
+          break;
+        case 'METER_MODEL':
+          obj.data.meterModel = hex2Ascii(msg.data.substr(dataIndex, 8));
+          break;
+        case 'POWER':
+          obj.data.power = hex2Dec(msg.data.substr(dataIndex, 8)) / 1000;
+          break;
+        case 'POWER_PRODUCTION':
+          obj.data.powerProduction = hex2Dec(msg.data.substr(dataIndex, 8)) / 1000;
+          break;
+        case 'POWER_REACTIVE':
+          obj.data.powerReactive = hex2Dec(msg.data.substr(dataIndex, 8)) / 1000;
+          break;
+        case 'POWER_PRODUCTION_REACTIVE':
+          obj.data.powerProductionReactive = hex2Dec(msg.data.substr(dataIndex, 8)) / 1000;
+          break;
+        case 'CURRENT_L1':
+          obj.data.currentL1 = hex2DecSign(msg.data.substr(dataIndex, 4)) / 10;
+          break;
+        case 'CURRENT_L2':
+          obj.data.currentL2 = hex2DecSign(msg.data.substr(dataIndex, 4)) / 10;
+          break;
+        case 'CURRENT_L3':
+          obj.data.currentL3 = hex2DecSign(msg.data.substr(dataIndex, 4)) / 10;
+          break;
+        case 'VOLTAGE_PHASE_1':
+          obj.data.voltagePhase1 = hex2Dec(msg.data.substr(dataIndex, 4)) / 10;
+          break;
+        case 'VOLTAGE_PHASE_2':
+          obj.data.voltagePhase2 = hex2Dec(msg.data.substr(dataIndex, 4)) / 10;
+          break;
+        case 'VOLTAGE_PHASE_3':
+          obj.data.voltagePhase3 = hex2Dec(msg.data.substr(dataIndex, 4)) / 10;
+          break;
+        case 'DATE':
+          let i = dataIndex;
+          obj.data.meterDate = getAmsTime(msg.data, i);
+          obj.listType = 'list3';
+          break;
+        case 'LAST_METER_CONSOMPTION':
+          obj.data.lastMeterConsumption = hex2Dec(msg.data.substr(dataIndex, 8)) / 100;
+          break;
+        case 'LAST_METER_PRODUCTION':
+          obj.data.lastMeterProduction = hex2Dec(msg.data.substr(dataIndex, 8)) / 100;
+          break;
+        case 'LAST_METER_CONSOMPTION_REACTIVE':
+          obj.data.lastMeterConsumptionReactive = hex2Dec(msg.data.substr(dataIndex, 8)) / 100;
+          break;
+        case 'LAST_METER_PRODUCTION_REACTIVE':
+          obj.data.lastMeterProductionReactive = hex2Dec(msg.data.substr(dataIndex, 8)) / 100;
+          break;
+      }
+    }
   }
 
   if (Object.getOwnPropertyNames(obj.data).length === 0) {
     console.error("Raw data packet exception : ", JSON.stringify(msg));
-  } 
-  /*
-  else {
-    //if (debug) console.log(obj)
-    obj.data.minPower = await getMinPower(obj.data.power);
-    obj.data.maxPower = await getMaxPower(obj.data.power);
-    // TODO: calculate average
-    obj.averagePower = 0;
   }
-  */
-  //if (debug) console.log(obj)
-  return obj;
-};
 
-const listHandler = async function (buf) {
+  return obj;
+}
+
+/**
+ * Handles the list data by decoding it and emitting an event.
+ * @param {Buffer} buf - The list data buffer to be handled.
+ */
+async function listHandler(buf) {
   let hex = await buf.toString('hex').toUpperCase();
-  let result = await listDecode(hex)
+  let result = await listDecode(hex);
   let listObject = result['data'];
   let list = result['listType'];
-  //if (list === 'list3')
   obj = await amsCalc.calc(listObject);
   event.emit(list, obj);
-};
+}
 
-event.on('pulse', listHandler)
+event.on('pulse', listHandler);
 
-module.exports = {listHandler};
+module.exports = { listHandler };
+
+
--- ams/kaifa.js
+++ ams/kaifa.js
@@ -1,10 +1,10 @@
 const yaml = require("yamljs");
 const configFile = "./config.yaml";
 
-const db = require('../misc/dbinit.js')
-const amsCalc = require('../ams/amscalc.js')
-const { event } = require('../misc/misc.js')
-const { hex2Dec, hex2Ascii, getAmsTime } = require('../misc/util.js')
+const db = require('../misc/dbinit.js');
+const amsCalc = require('../ams/amscalc.js');
+const { event } = require('../misc/misc.js');
+const { hex2Dec, hex2Ascii, getAmsTime } = require('../misc/util.js');
 
 // Load broker and topics preferences from config file
 const config = yaml.load(configFile);
@@ -13,108 +13,91 @@ const debug = config.DEBUG;
 let len;
 let obj = new Object();
 
-/*
-function getMinPower(pow) {
-  if (db.get('minPower') === undefined || db.get('minPower') > pow){
-    db.set('minPower', pow);
-    db.sync();
-  }
-  return db.get('minPower');
-};
-
-function getMaxPower(pow) {
-  if (db.get('maxPower') === undefined || db.get('maxPower') < pow) {
-    db.set('maxPower', pow);
-    db.sync();
-  }
-  return db.get('maxPower');
-};
-*/
-
+/**
+ * Decode the list message and extract relevant information
+ * @param {string} msg - Hexadecimal message string to be decoded
+ * @returns {Object} - An object containing the decoded list information and the list type
+ */
 const listDecode = async function (msg) {
   let listType = 1;
   let elements = 0;
-  let index = msg.indexOf("FF800000") + 8
-  elements = hex2Dec(msg.substr((index + 2), 2))
+  let index = msg.indexOf("FF800000") + 8;
+  elements = hex2Dec(msg.substr((index + 2), 2));
   obj = {};
-  obj.timestamp = getAmsTime(msg, 38)
+  obj.timestamp = getAmsTime(msg, 38);
 
+  // Process the elements based on their count
   if (elements === 1) {
-    listType = 'list1'
-    obj.power = hex2Dec(msg.substr(index + 6, 8)) / 1000;
-  };
+      listType = 'list1';
+      obj.power = hex2Dec(msg.substr(index + 6, 8)) / 1000;
+  }
 
   if (elements >= 9) {
-    index = index + 6
-    len = hex2Dec(msg.substr(index, 2)) * 2
-    obj.meterVersion = hex2Ascii(msg.substr(index + 2, len))
-    index += 4 + len;
-    len = hex2Dec(msg.substr(index, 2)) * 2
-    obj.meterID = hex2Ascii(msg.substr(index + 2, len))
-    index += 4 + len;
-    len = hex2Dec(msg.substr(index, 2)) * 2
-    obj.meterModel = hex2Ascii(msg.substr(index + 2, len))
-    index += 4 + len;
-    obj.power = hex2Dec(msg.substr(index, 8)) / 1000;
-    obj.powerProduction = hex2Dec(msg.substr(index += 10, 8)) / 1000;
-    obj.powerReactive = hex2Dec(msg.substr(index += 10, 8)) / 1000;
-    obj.powerProductionReactive = hex2Dec(msg.substr(index += 10, 8)) / 1000;
-  };
+      index = index + 6;
+      len = hex2Dec(msg.substr(index, 2)) * 2;
+      obj.meterVersion = hex2Ascii(msg.substr(index + 2, len));
+      index += 4 + len;
+      len = hex2Dec(msg.substr(index, 2)) * 2;
+      obj.meterID = hex2Ascii(msg.substr(index + 2, len));
+      index += 4 + len;
+      len = hex2Dec(msg.substr(index, 2)) * 2;
+      obj.meterModel = hex2Ascii(msg.substr(index + 2, len));
+      index += 4 + len;
+      obj.power = hex2Dec(msg.substr(index, 8)) / 1000;
+      obj.powerProduction = hex2Dec(msg.substr(index += 10, 8)) / 1000;
+      obj.powerReactive = hex2Dec(msg.substr(index += 10, 8)) / 1000;
+      obj.powerProductionReactive = hex2Dec(msg.substr(index += 10, 8)) / 1000;
+  }
 
   if (elements === 9 || elements === 14) {
-    //console.log('Index type 2', index)
-    listType = 'list2'
-    index += 0;
-    obj.currentL1 = hex2Dec(msg.substr(index += 10, 8)) / 1000;
-    obj.voltagePhase1 = hex2Dec(msg.substr(index += 10, 8)) / 10;
-  };
+      listType = 'list2';
+      index += 0;
+      obj.currentL1 = hex2Dec(msg.substr(index += 10, 8)) / 1000;
+      obj.voltagePhase1 = hex2Dec(msg.substr(index += 10, 8)) / 10;
+  }
 
   if (elements === 13 || elements === 18) {
-    listType = 'list2'
-    index += 0;
-    obj.currentL1 = hex2Dec(msg.substr(index += 10, 8)) / 1000;
-    obj.currentL2 = hex2Dec(msg.substr(index += 10, 8)) / 1000;
-    obj.currentL3 = hex2Dec(msg.substr(index += 10, 8)) / 1000;
-    obj.voltagePhase1 = hex2Dec(msg.substr(index += 10, 8)) / 10;
-    obj.voltagePhase2 = hex2Dec(msg.substr(index += 10, 8)) / 10;
-    obj.voltagePhase3 = hex2Dec(msg.substr(index += 10, 8)) / 10;
+      listType = 'list2';
+      index += 0;
+      obj.currentL1 = hex2Dec(msg.substr(index += 10, 8)) / 1000;
+      obj.currentL2 = hex2Dec(msg.substr(index += 10, 8)) / 1000;
+      obj.currentL3 = hex2Dec(msg.substr(index += 10, 8)) / 1000;
+      obj.voltagePhase1 = hex2Dec(msg.substr(index += 10, 8)) / 10;
+      obj.voltagePhase2 = hex2Dec(msg.substr(index += 10, 8)) / 10;
+      obj.voltagePhase3 = hex2Dec(msg.substr(index += 10, 8)) / 10;
 
-    if (obj.voltagePhase2 === 0) // This meter doesn't measure L1L3
-      obj.voltagePhase2 = (Math.sqrt((obj.voltagePhase1 - obj.voltagePhase3 * 0.5) ** 2 + (obj.voltagePhase3 * 0.866) ** 2)).toFixed(0) * 1;
-  };
+      if (obj.voltagePhase2 === 0) {
+          obj.voltagePhase2 = (Math.sqrt((obj.voltagePhase1 - obj.voltagePhase3 * 0.5) ** 2 + (obj.voltagePhase3 * 0.866) ** 2)).toFixed(0) * 1;
+      }
+  }
 
   if (elements === 14 || elements === 18) {
-    //console.log('Index type 3', index)
-    listType = 'list3'
+      listType = 'list3';
+      obj.meterDate = getAmsTime(msg, index += 12);
+      index += 14;
+      obj.lastMeterConsumption = hex2Dec(msg.substr(index += 12, 8)) / 1000;
+      obj.lastMeterProduction = hex2Dec(msg.substr(index += 10, 8)) / 1000;
+      obj.lastMeterConsumptionReactive = hex2Dec(msg.substr(index += 10, 8)) / 1000;
+      obj.lastMeterProductionReactive = hex2Dec(msg.substr(index += 10, 8)) / 1000;
+    };
 
-    obj.meterDate = getAmsTime(msg, index += 12);
-    index += 14;
-    obj.lastMeterConsumption = hex2Dec(msg.substr(index += 12, 8)) / 1000;
-    obj.lastMeterProduction = hex2Dec(msg.substr(index += 10, 8)) / 1000;
-    obj.lastMeterConsumptionReactive = hex2Dec(msg.substr(index += 10, 8)) / 1000;
-    obj.lastMeterProductionReactive = hex2Dec(msg.substr(index += 10, 8)) / 1000;
-  };
-
-  /*
-  obj.minPower = await getMinPower(obj.power);
-  obj.maxPower = await getMaxPower(obj.power);
-  // TODO: calculate average
-  obj.averagePower = 0;
-  */
-  let ret = { "data": obj, "list": listType }
-  return ret;
+    return { "data": obj, "list": listType };
 };
 
+/**
+ * Handle the list messages, decode them and emit the corresponding event
+ * @param {Buffer} buf - Buffer containing the list message
+ */
 const listHandler = async function (buf) {
-  let hex = await buf.toString('hex').toUpperCase();
-  let result = await listDecode(hex)
-  let listObject = result['data'];
-  let list = result['list'];
-  //if (list === 'list3')
-  obj = await amsCalc.calc(list, listObject);
-  event.emit(list, obj);
+    let hex = await buf.toString('hex').toUpperCase();
+    let result = await listDecode(hex);
+    let listObject = result['data'];
+    let list = result['list'];
+
+    obj = await amsCalc.calc(list, listObject);
+    event.emit(list, obj);
 };
 
-event.on('pulse', listHandler)
+event.on('pulse', listHandler);
 
-module.exports = {listHandler};
+module.exports = { listHandler };
